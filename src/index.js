d68f38a84b1cfe6a108bc610c55e0070f5e175c0
github username <github email address>
2020-08-01 15:21:30 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+ let breedList = [];
+
 // Challenge 1
 // Add JavaScript so that:
 // on page load
@@ -43,12 +45,17 @@
 // .then(function(json) {
 //   console.log(json)
 // });
+
+// Once all of the breeds are rendered in the <ul>, add JavaScript so that the font color of a particular <li> changes on click. This can be a color of your choosing.
+// When the user clicks any of the dog breed list items, the color the text should change.
  
 
 function fetchImgB() {
     return fetch('https://dog.ceo/api/breeds/list/all')
     .then(resp => resp.json())
-    .then(json => renderImgB(json));
+    .then(json => 
+        // breedList.push(json);
+        renderImgB(json));
   }
 
 
@@ -56,9 +63,12 @@ function fetchImgB() {
   function renderImgB(json) {
     //   debugger
    const ul = document.querySelector("#dog-breeds")
- Object.keys(json.message).forEach(breedUrl => {
+ breedList = Object.keys(json.message);
+
+ breedList.forEach(breedUrl => {
   const imgB = document.createElement('li')
   imgB.innerText= breedUrl
+  imgB.addEventListener('click', _ => imgB.style = ('color: red'));
   ul.appendChild(imgB)
 })
 }
@@ -67,11 +77,6 @@ document.addEventListener('DOMContentLoaded', function() {
     fetchImgB()
 })
 
-// Once all of the breeds are rendered in the <ul>, add JavaScript so that the font color of a particular <li> changes on click. This can be a color of your choosing.
-// When the user clicks any of the dog breed list items, the color the text should change.
-
-  document.querySelectorAll('li').forEach(li => li.addEventListener('click', _ => li.style.color = 'red'));
-  var ul = document.getElementById("list");
 
 
 
@@ -79,23 +84,19 @@ document.addEventListener('DOMContentLoaded', function() {
 // Once we are able to load all of the dog breeds onto the page, add JavaScript so that the user can filter breeds that start with a particular letter using a dropdown.
 // For example, if the user selects 'a' in the dropdown, only show the breeds with names that start with the letter a. For simplicity, the dropdown only includes the letters a-d. However, we can imagine expanding this to include the entire alphabet
 
-function myFunction() {
-    document.getElementById("breed-dropdown").classList.toggle("select-breed");
-  }
-  
-  function filterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("select-breed");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("breed-dropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-      txtValue = a[i].textContent || a[i].innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
-      } else {
-        a[i].style.display = "none";
-      }
+
+
+
+
+
+//         if (breedList[0][0]== event.target.value) {
+    
+document.getElementById('submit').onclick = function() {
+  var selected = [];
+  for (var option of document.getElementById('breed-dropdown').options) {
+    if (option.selected) {
+      selected.push(option.value);
     }
   }
-
+  alert(selected);
+}

2311409dbe850f2d6e123456b65265a3ac5ca53e
kelly_manahan <kelly.a.manahan@gmail.com>
2020-08-03 14:11:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () => {
+    // renderImg();
+    renderAll();
+});
+
+function renderImg() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(result =>result.message.forEach(img => renderdog(img))); 
+};
+
+function renderdog(dog) {
+    let container = document.getElementById('dog-image-container');
+    let newImg = document.createElement('img');
+    newImg.src = dog;
+    container.appendChild(newImg)
+};
+
+function renderAll() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+    fetch(breedUrl)
+        .then(resp => resp.json())
+        .then(result =>result.message.forEach(index => addBreed(index))); 
+};
+
+function addBreed(breed) {
+    let list = document.getElementById('dog-breeds')
+    let li = document.createElement('li');
+    li.li = breed
+    list.appendChild(li);
+    
+}
+

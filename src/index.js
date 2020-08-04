c741be23b08d2667c90a81bb577a8ea50ef5823a
kelly_manahan <kelly.a.manahan@gmail.com>
2020-08-04 10:46:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // renderImg();
+    dropDown();
     renderAll();
 });
 
@@ -22,7 +23,6 @@ function renderAll() {
     fetch(breedUrl)
         .then(resp => resp.json())
         .then(result =>{
-       
             breeds = Object.keys(result.message).forEach(breed => addBreed(breed))
             }); 
 };
@@ -30,7 +30,29 @@ function renderAll() {
 function addBreed(breed) {
     let list = document.getElementById('dog-breeds');
     let li = document.createElement('li');
-    li.li = breed;
+    li.innerHTML = breed;
     list.appendChild(li);
+    li.addEventListener('click', event => {
+        event.target.style.color = "purple";
+    })
+}
+
+function dropDown() {
+    let li= document.querySelector('li');
+    let drop = document.getElementById('breed-dropdown');
+    drop.addEventListener('click', event => {
+        if(event.target.value == 'a') {
+        li.filter((breed) => breed.startsWith('a'));
+        };
+        if(event.target.value == 'b') {
+            console.log('b');
+        };
+        if(event.target.value == 'c') {
+            console.log('c');
+        };    
+        if(event.target.value == 'd') {
+            console.log('d');
+        };
+    });
 }
 

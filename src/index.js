e786dcb410b2f07f21377c1506f87f3823b790d5
kelly_manahan <kelly.a.manahan@gmail.com>
2020-08-04 12:52:25 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -24,7 +24,9 @@ function renderAll() {
         .then(resp => resp.json())
         .then(result =>{
             breeds = Object.keys(result.message).forEach(breed => addBreed(breed))
-            }); 
+        dropDown(result.message);    
+        }); 
+            
 };
 
 function addBreed(breed) {
@@ -37,22 +39,44 @@ function addBreed(breed) {
     })
 }
 
-function dropDown() {
-    let li= document.querySelector('li');
+function dropDown(message) {
     let drop = document.getElementById('breed-dropdown');
     drop.addEventListener('click', event => {
-        if(event.target.value == 'a') {
-        li.filter((breed) => breed.startsWith('a'));
-        };
-        if(event.target.value == 'b') {
-            console.log('b');
-        };
-        if(event.target.value == 'c') {
-            console.log('c');
-        };    
-        if(event.target.value == 'd') {
-            console.log('d');
-        };
+        // Clear any children elements in the <ul id="dog-breeds">
+        dogBreeds = document.getElementById('dog-breeds');
+        dogBreeds.innerHTML = '';
+      // Then readd li elements based on what we are able to parse 
+      // using the '.filter()' function on our collection of breeds
+      for(const breedName in message) {
+          if(breedName.startsWith(event.target.value)) {
+            let li = document.createElement('li');
+            li.innerHTML = breedName;
+            dogBreeds.appendChild(li);
+          }
+      }
+      
+    //   if(event.target.value == 'a') {
+    //         console.log(list)
+    //     list.filter((breed) => breed.startsWith('a'));
+    //     };
+    //     if(event.target.value == 'b') {
+    //         console.log('b');
+    //     };
+    //     if(event.target.value == 'c') {
+    //         console.log('c');
+    //     };    
+    //     if(event.target.value == 'd') {
+    //         console.log('d');
+    //     };
     });
 }
+
+// function dropDown() {
+//     let li= document.querySelector('li');
+//     let drop = document.getElementById('breed-dropdown');
+    
+//     drop.addEventListener('click', event => {
+      
+//     });
+//   }
 
